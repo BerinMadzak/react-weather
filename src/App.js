3bf5e9e41d2d551a14a286bb0ba2060a11e6b04e
TGS Berin <berinmadzak@gmail.com>
2023-06-22 11:49:35 +0200

    Created base form functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
+import WeatherForm from './modules/WeatherForm';
 
 function App() {
+  const [city, setCity] = useState("");
+
+  const changeCity = (e) => {
+    setCity(e.target.value);
+  }
+
+  const updateWeather = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-     
+     <h1>Weather Forecast</h1>
+     <WeatherForm changeCity={changeCity} updateWeather={updateWeather}/>
+     <h1>{city}</h1>
     </div>
   );
 }
---
+++ src/modules/WeatherForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function WeatherForm(props) {
+
+
+
+    return (
+        <div>
+        <form onSubmit={props.updateWeather}>
+            <input type="text" placeholder="Enter a city" onChange={props.changeCity}/>
+            <button type="submit">Get Weather</button>
+        </form>
+        </div>
+    )
+}
